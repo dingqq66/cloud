6c747f3e460158a4d80916fee4dd8eb8ed8c3368
Administrator <Administrator@192.168.199.193>
2020-02-07 11:21:54 +0800

    ww

---
+++ sp09-feign-1/src/main/java/cn/tedu/sp09/Sp09Feign1Application.java
@@ -0,0 +1,13 @@
+package cn.tedu.sp09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp09Feign1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp09Feign1Application.class, args);
+	}
+
+}
